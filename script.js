bddfe211acb1225b72fc50576b3f7eec0fc4d501
Mak-3 <75625675+Mak-3@users.noreply.github.com>
2023-02-25 11:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ buttons.forEach((button)=>{
         if(e.target.innerText ==="." && (operations.includes(inputs[inputs.length-1]))){
             return;
         }
+        if((operations.includes(e.target.innerText)) && inputs[inputs.length-1]=="."){
+            return;
+        }
         if((inputs.length==0) && (operations.includes(e.target.innerText))){
             return;
         }
@@ -56,9 +59,24 @@ function clearlast(){
     display2.innerText = temp;
 }
 function evaluate(){
+    let counter = 0;
+    let counter2 = 0;
     if(operations.includes(inputs[inputs.length-1])){
-        display2_num="Invalid Input";
-        display2.innerHTML=display2_num;
+        display2_num=display2_num.slice(0,-1);
+        display2.innerHTML=eval(display2_num);
+    }
+    for(let i=0;i<inputs.length;i++){
+        if(operations.includes(inputs[i])){
+            counter++;
+        }
+    }
+    for(let i=0;i<inputs.length;i++){
+        if(inputs[i] == "."){
+            counter2++;
+        }
+    }
+    if(counter2>counter){
+        display2.innerHTML="Invalid input";
     }
     result = eval(display2_num.replaceAll("x",replacex));
     display1.innerHTML=display2_num;
